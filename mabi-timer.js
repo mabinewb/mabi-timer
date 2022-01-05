739f0c323ecd238349b3eadf3c1ce2bfd6d0b062
mabinewb <user@khu.ac.kr>
2022-01-05 20:29:49 +0900

    번쩍번쩍

--- mabi-timer.js
+++ mabi-timer.js
@@ -19,21 +19,21 @@ function update_clock(){
     let sailtime = clocktype.value == "iria" ? get_sailtime() : get_sailtime("Sun Jan 02 2022 04:24:59 GMT+0900", 6);
     let remain = sailtime.getTime() - now.getTime();
     if(remain <= 60*1000){
-        document.querySelector("#remain").className = "red";
+        document.querySelector("#container").className = "red";
     } else if(remain <= 120*1000){
-        document.querySelector("#remain").className = "orange";
+        document.querySelector("#container").className = "orange";
     } else {
-        document.querySelector("#remain").className = "";
+        document.querySelector("#container").className = "";
     }
     let remain_min = parseInt(remain / 1000 / 60);
     let remain_sec = parseInt(remain / 1000 % 60);
     document.querySelector("#left").innerHTML = clocktype.value == "iria" ? "이리아" : "벨바스트";
     document.querySelector("#left").className = clocktype.value == "iria" ? "iria" : "belfast";
     document.querySelector("#now").innerHTML = "현재 시간 : " + now.format("yy-mm-dd HH:MM:ss");
-    document.querySelector("#nowerin").innerHTML = "(현재 에린 시간 : " + erin(now) + ")";
+    document.querySelector("#nowerin").innerHTML = "( 현재 에린 시간 : " + erin(now) + " )";
     document.querySelector("#nextsail").innerHTML = "다음 출항 시간 : " + sailtime.format("yy-mm-dd HH:MM:ss");
-    document.querySelector("#erintime").innerHTML = "(출항 에린 시간 : " + erin(sailtime) + ")";
-    document.querySelector("#erintime_arrival").innerHTML = "(도착 에린 시간 : " + (clocktype.value == "iria" ? erin(sailtime, 4 * 60) : erin(sailtime, 2 * 60)) + ")";
+    document.querySelector("#erintime").innerHTML = "( 출항 에린 시간 : " + erin(sailtime) + " )";
+    document.querySelector("#erintime_arrival").innerHTML = "( 도착 에린 시간 : " + (clocktype.value == "iria" ? erin(sailtime, 4 * 60) : erin(sailtime, 2 * 60)) + " )";
     document.querySelector("#remain").innerHTML = "출항까지 남은 시간 : " + (remain_min == 0 ? "" : remain_min + "분 ") + remain_sec + "초";
 }
 
